95fb82b77c4a4dff2a076d2aba03d9da79fba0b8
Ishaan <ishaanrejra@gmail.com>
2022-12-07 09:53:50 +0530

    Create character-input.java

---
+++ character-input.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class Main
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+      	char ch = sc.next().charAt(0);
+      	System.out.print(ch);
+    }
+}
