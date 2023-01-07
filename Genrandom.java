ccf3a9cb324a0896061569ee76793a2536b6664b
Ishaan <ishaanrejra@gmail.com>
2023-01-07 22:06:51 +0530

    Create Genrandom.java

---
+++ Genrandom.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Genrandom
+{
+  public static void main(String[] a)
+  {
+    System.out.println("Ist number is " + Math.random());
+    System.out.println("2nd number is " + Math.random());
+    System.out.println("3rd number is " + Math.random());
+    System.out.println("4th number is " + Math.random());
+  }
+}
